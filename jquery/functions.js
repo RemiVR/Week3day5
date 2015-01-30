3e894aafcc79819ae228c1703e7f3e3710e9cd3f
RemiVR <remi.vr5@gmail.com>
2015-01-30 12:31:09 +0100

    inbox content

--- jquery/functions.js
+++ jquery/functions.js
@@ -13,7 +13,10 @@ $("#blank-close").bind("click",function(){
 $("#trash").bind("click", function(){
 	alert("Message has been deleted");
 	$("#blank-email").hide();
+	$("#new_email input").val("");
+	$("#new_email textarea").val("");
 });
+
 $("#send").bind("click", function(){
 	$.post(SERVER + "/email", function(data){
 		alert("Email sent");
@@ -22,3 +25,22 @@ $("#send").bind("click", function(){
 		$("#new_email textarea").val("");
 	});
 });
+
+$("#inbox").bind("click", function(){
+	$.get(SERVER + "/email/new", function(data){
+		$.each(data, function(emailId, email){
+			console.log(email.subject)
+		})
+		// console.log(data)
+	});
+});
+
+var emails = {
+	112233: {
+		subject: 'my subject...',
+
+	}
+};
+
+
+
